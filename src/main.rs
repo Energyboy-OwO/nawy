d0871e1b9e078b66882cd07e035bda9bc9d9e855
Energyboy-OwO <94549302+Energyboy-OwO@users.noreply.github.com>
2026-07-16 11:53:17 +0100

    import thingadoodads

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // nawy
 // my first attempt at a discord bot written in rust, lord save us all
-
-
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use std::env;
 
 fn main() {
     println!("Hello, world!");
